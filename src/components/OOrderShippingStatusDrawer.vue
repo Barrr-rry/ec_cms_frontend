39cf89ce866c215d7cfb3b16dd4717fecb41a737
barry <barry@conquers.co>
2020-08-14 14:06:11 +0800

    fix: 刪除管理帳號時，要同時刪掉其token
    (cherry picked from commit 8f2de3091c78668139e01f9036178ba0558d1a0d)
    
    # Conflicts:
    #	web/api/views.py

---
+++ src/components/OOrderShippingStatusDrawer.vue
@@ -0,0 +1,55 @@
+<template>
+  <c-drawer v-model="input" title="出貨"
+            @ok="submitHandler"
+            okText="確 定 出 貨"
+  >
+    <h3>出貨內容</h3>
+    <a-form :form="form" @submit="submitHandler">
+      <c-form-item label="出貨單號">
+        <a-input
+          v-decorator="['all_pay_logistics_id', { rules: [
+            { required: true, message: '請輸入出貨單號' },
+            ]}]"
+          placeholder="請輸入出貨單號"
+          :disabled="!editPermissioncheck()"
+        />
+      </c-form-item>
+    </a-form>
+  </c-drawer>
+
+</template>
+
+<script>
+  import drawerMixin from "@/mixins/drawerMixin"
+  import uploadMixin from "@/mixins/uploadMixin"
+
+  export default {
+    mixins: [drawerMixin, uploadMixin],
+    props: {
+      main_category: {
+        type: Number,
+        default: null
+      }
+    },
+    data() {
+      return {
+        update_field_keys: ['all_pay_logistics_id'],
+        default_api: this.$api.order,
+        // for check to add
+        // fake_data: {}
+      }
+    },
+    computed: {},
+    methods: {
+      updateValueTransfer(values) {
+        values.shipping_status = 2
+        values.simple_status = 2
+        values.simple_status_display = '已出貨'
+        return values
+      },
+    }
+  }
+</script>
+
+<style scoped>
+</style>
